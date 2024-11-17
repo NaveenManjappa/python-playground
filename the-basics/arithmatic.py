84e434a4b865891139532883063861425a399c89
Naveen Manjappa <NManjappa@manh.com>
2024-11-17 20:19:58 +0000

    the-basics basic operators

---
+++ the-basics/arithmatic.py
@@ -0,0 +1,27 @@
+# arithmatic = + addition
+#              - subtraction
+#              * multiplication
+#              / division (returns a float)
+#              // integer division (returns an integer)
+#              % remainder
+
+friends = 5
+print(friends)
+
+friends += 1
+print(friends)
+
+friends -= 1
+print(friends)
+
+friends *=3
+print(friends)
+
+friends /= 2
+print(friends)
+
+friends //=3
+print(friends)
+
+remaining_friends = 10 % 3
+print(remaining_friends)
