7682b8cf7bcf99b30a9a593076e6e7bae14a4af9
Naveen Manjappa <NManjappa@manh.com>
2024-11-18 07:31:29 +0000

    the-basics user input

---
+++ the-basics/userinput.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello, {name}!")
+print(type(name))
+print(f"You are {age} years old!")
+print(type(age))
+
+#age = int(age)
+age += 1
+print(age)
