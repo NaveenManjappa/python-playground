6c90db6977dd0153a8ab9af4043942c36b58990c
Naveen Manjappa <NManjappa@manh.com>
2024-11-19 13:03:58 +0000

    the-basics for loop

---
+++ the-basics/for.py
@@ -0,0 +1,17 @@
+# for loop = used to iterate over a sequence (string, list, tuple,set)
+# repeat a block of code an exact amount of times
+
+import time
+
+for i in range(1,11,4):
+    print(i)
+
+name = "Bro code"
+for letter in name:
+    print(letter,end="-")
+
+for i in range(10,0,-1):
+    print(i)
+    time.sleep(1)
+
+print("HAPPY NEW YEAR!")
